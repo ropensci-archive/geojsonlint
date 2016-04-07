8a983207f1ff0e2de5a56be4b54e0b3234c856a0
ateucher <andy.teucher@gmail.com>
2016-04-06 23:02:51 -0700

    Add other classes for geojson_validate

--- R/geojson_validate.R
+++ R/geojson_validate.R
@@ -1,6 +1,7 @@
 #' geojson_validate
 #'
-#' @param json the json object
+#' @param x Input, a geojson character string, json object, or file or
+#' url pointing to one of the former
 #' @param verbose TRUE or FALSE
 #' @param greedy TRUE or FALSE
 #' @param error TRUE or FALSE
@@ -10,13 +11,36 @@
 #' @return TRUE or FALSE
 #' @export
 #'
-geojson_validate <- function(json, verbose = FALSE, greedy = FALSE, error = FALSE) {
+geojson_validate <- function(x, verbose = FALSE, greedy = FALSE, error = FALSE) {
   UseMethod("geojson_validate")
 }
 
 #' @export
-geojson_validate.json <- function(json, verbose = FALSE, greedy = FALSE, error = FALSE) {
-  validate_geojson(json = json, verbose = verbose, greedy = greedy, error = error)
+geojson_validate.default <- function(x, verbose = FALSE, greedy = FALSE, error = FALSE) {
+  stop("no geojson_validate method for ", class(x), call. = FALSE)
+}
+
+#' @export
+geojson_validate.character <- function(x, verbose = FALSE, greedy = FALSE, error = FALSE) {
+  validate_geojson(json = x, verbose = verbose, greedy = greedy, error = error)
+}
+
+#' @export
+geojson_validate.location <- function(x, verbose = FALSE, greedy = FALSE, error = FALSE){
+  res <- switch(attr(x, "type"),
+                file = paste0(readLines(x), collapse = ""),
+                url = jsonlite::minify(httr::content(httr::GET(x), "text", encoding = "UTF-8")))
+  validate_geojson(json = x, verbose = verbose, greedy = greedy, error = error)
+}
+
+#' @export
+geojson_validate.geojson <- function(x, verbose = FALSE, greedy = FALSE, error = FALSE){
+  validate_geojson(json = unclass(x), verbose = verbose, greedy = greedy, error = error)
+}
+
+#' @export
+geojson_validate.json <- function(x, verbose = FALSE, greedy = FALSE, error = FALSE) {
+  validate_geojson(json = x, verbose = verbose, greedy = greedy, error = error)
 }
 
 validate_geojson <- jsonvalidate::json_validator(system.file("schema/geojson.json", package = "geojsonlint"))
